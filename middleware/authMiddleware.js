2232760a238aeb4aac970b38fcb8fd1015a86311
Panucci <eyakubu442@gmail.com>
2025-04-22 08:32:06 -0700

    checkingCurrentUser

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,7 +5,7 @@ const requireAuth = (req, res, next) => {
 
   // check json web token exists & is verified
   if (token) {
-    jwt.verify(token, 'switch secret', (err, decodedToken) => {
+    jwt.verify(token, 'net ninja secret', (err, decodedToken) => {
       if (err) {
         console.log(err.message);
         res.redirect('/login');
@@ -19,4 +19,25 @@ const requireAuth = (req, res, next) => {
   }
 };
 
-module.exports = { requireAuth };
+// check current user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, 'net ninja secret', async (err, decodedToken) => {
+      if (err) {
+        res.locals.user = null;
+        next();
+      } else {
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+
+module.exports = { requireAuth, checkUser };
